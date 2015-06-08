a2e2df881b0660776a398a3e111b8339d35fb167
Fabio Quinzi <fabio.quinzi@gmail.com>
2015-06-08 19:24:12 +0200

    Some update to the plugin

--- just-a-check.php
+++ just-a-check.php
@@ -5,11 +5,11 @@
  */
 /*
 Plugin Name: Just a Check
-Plugin URI: http://fabioquinzi.com/plugins/just-a-check/
+Plugin URI: #
 Description: This is just a test plugin I'm coding to check some updates
-Author: Fabio Quinzi
+Author: Fabio Quinzi and Michele Falconi
 Version: 0.1
-Author URI: http://fabioquinzi.com/
+Author URI: #
 */
 
 add_action('admin_menu', 'justacheck_admin_actions');
@@ -19,7 +19,52 @@ function justacheck_admin_actions() {
 }
 
 function justacheckplugin_admin(){
+?>
+<div class="wrap">
+<h4>This plugin, hopefully, will show if someone stole content from your posts</h4>
+<table class="widefat">
+	<thead>
+		<tr>
+			<th>Post Title</th>
+			<th>Post ID</th>
+		</tr>
+	</thead>
+	<tfoot>
+		<tr>
+			<th>Post Title</th>
+			<th>Post ID</th>
+		</tr>
+	</tfoot>
+	<tbody>
 
+	<?php
+		global $wpdb;
+
+		$mypoststoscan = $wpdb->get_results(
+				"
+				SELECT ID, post_title
+				FROM $wpdb->posts
+				WHERE post_status = 'publish'
+				AND post_type='post'
+				"
+			);
+	?>
+	<?php
+		foreach ($mypoststoscan as $myposttoscan)
+	{
+	?>
+	<tr>
+	<?php
+		echo "<td>".$myposttoscan->post_title."</td>";
+		echo "<td>".$myposttoscan->ID."</td>";
+	?>
+	</tr>
+	<?php
+	}
+	?>
+	</tbody>
+</table>
+</div>
+<?php
 }
-
 ?>
